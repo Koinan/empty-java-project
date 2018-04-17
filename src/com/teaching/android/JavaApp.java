a3103824e71f1212902927783533b5918df74e1a
Nando <fersamed89@gmail.com>
2018-04-17 20:47:36 +0200

    Nuevo intento fallido

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -9,26 +9,31 @@ import java.util.Scanner;
 public class JavaApp {
 
     public static void main(String[] args) {
+
+
         Random generator = new Random();
         int random = generator.nextInt(10) + 1;
 
         Scanner keyboard = new Scanner(System.in);
         int  valor = keyboard.nextInt();
-
-
+        int a = 0;
         do   {
             if (valor > random) {
-                System.out.println("menor");
+                System.out.println("Es menor");
+                a++;
             } else if (valor < random) {
-                System.out.println("mayor");
+                System.out.println("Es mayor");
+                a++;
+            } else if (a > 3) {
+                    System.out.println("Juego terminado, has perdido");
+                    break;
             } else {
-                System.out.println("has ganado");
+                System.out.println("Has ganado");
             }
             valor = keyboard.nextInt();
         }
-        while (valor != random);
-        System.out.println("Has ganado");
+        while (valor != random || a != 3);
+        }
 
 
     }
-}
