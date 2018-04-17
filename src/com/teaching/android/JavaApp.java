f9f91efe21fceb3d060fb1857730be7f2faaae14
Nando <fersamed89@gmail.com>
2018-04-17 14:08:35 +0200

    Sumas, multiplicaciones, restas etc

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -12,19 +12,15 @@ public class JavaApp {
         short s=30; //16 bits. el número se lo indico yo
         int i=65; //32 bits.
         long l=6555555L; //64 bits.
-        float f=6.77f; //32 bits. float necesita terminar en f por ser de 32 bits
+        float f=6.66f; //32 bits. float necesita terminar en f por ser de 32 bits
         double d=65.78; //64 bits. no necesita letra
         boolean bol= false; //1 bit
 
-        System.out.println(h);
-        System.out.println(c);
-        System.out.println(s);
-        System.out.println(i);
-        System.out.println(l);
-        System.out.println(f);
-        System.out.println(d);
-        System.out.println(bol);
+        System.out.println(2+5-3*2);
+        System.out.println(2*4*2/2);
+        System.out.println(1/2);
+        System.out.println(5*(1/2)); //los decimales sólo salen si ponemos el tipo float
+        System.out.println(5f*1/2);
 
     }
-
 }
