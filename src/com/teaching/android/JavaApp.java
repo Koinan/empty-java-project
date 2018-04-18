274a0fdd748b8beade3c295b24f9b036101c31de
Nando <fersamed89@gmail.com>
2018-04-18 11:22:21 +0200

    Bucle con for, numeros pares

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,39 +1,18 @@
 package com.teaching.android;
 
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  * Empty Java App for teaching purposes.
  */
 public class JavaApp {
 
     public static void main(String[] args) {
-        Random generator = new Random();
-        int random = generator.nextInt(10) + 1;
 
-    Scanner keyboard = new Scanner(System.in);
-    int  valor = keyboard.nextInt();
-    int fail = 0;
+    int i = 0;
 
-
-        do   {
-        if (valor > random) {
-            System.out.println("menor");
-            fail++;
-        } else if (valor < random) {
-            System.out.println("mayor");
-            fail++;
-        } else if (valor == random) {
-            System.out.println("has ganado");
-            System.exit(0);
+        for (i = 0; i < 20 +1; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
         }
-            valor = keyboard.nextInt();
-
-        }
-        while (fail < 2);
-        System.out.println("  3 intentos usados" );
-
-
     }
 }
